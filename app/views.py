30d7ea12eb71bd2dced72d2caa8b5305c1dbcf1d
Romario Lawes <romario.lawes@mymona.uwi.edu>
2018-03-03 20:52:58 +0000

    Step 9

--- app/views.py
+++ app/views.py
@@ -50,11 +50,16 @@ def login():
                 login_user(user)
                 # remember to flash a message to the user
                 flash('Logged in successfully.', 'success')
-                return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
             else:
                 flash('Incorrect Username or Password.', 'danger')
     return render_template("login.html", form=form)
 
+
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template("secure_page.html")
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
